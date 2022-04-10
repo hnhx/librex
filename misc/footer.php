8b6c0ca6f1c1fbd7dcd52c7be20ae88bb9ca85e4
Hanna <49120638+hnhx@users.noreply.github.com>
2022-04-10 11:28:34 +0200

    Revert "Subdir support and gitignore config.php"

--- misc/footer.php
+++ misc/footer.php
@@ -1,10 +1,10 @@
 <div class="footer-container">
-    <a href="./">LibreX</a>
+    <a href="/">LibreX</a>
     <a href="https://github.com/hnhx/librex/" target="_blank">Source &amp; Instance list</a>
-    <a href="./settings.php">Settings</a>
+    <a href="/settings.php">Settings</a>
       <a href="https://based-or-botnet.neocities.org/" target="_blank">Privacy guides</a>
-    <a href="./api.php" target="_blank">API</a>
-    <a href="./donate.php">Donate ❤️</a>
+    <a href="/api.php" target="_blank">API</a>
+    <a href="/donate.php">Donate ❤️</a>
 </div>
 </body>
 </html>
