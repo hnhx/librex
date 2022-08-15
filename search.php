af40ad7be817f556561a9cd2d19cdf216140be6e
Extravi <98912029+Extravi@users.noreply.github.com>
2022-08-14 22:18:31 -0400

    Update search.php

--- search.php
+++ search.php
@@ -4,7 +4,7 @@
 </head>
     <body>
         <form class="sub-search-container" method="get" autocomplete="off">
-            <h1 class="logomobile"><a class="noDecoration" href="#">Libre<span class="X">X</span></a></h1>
+            <h1 class="logomobile"><a class="noDecoration" href="./">Libre<span class="X">X</span></a></h1>
             <a href="./"><img class="logo" src="static/images/librex.png" alt="librex logo"></a>
             <input type="text" name="q" 
                 <?php
