bac0156e005d6868d0248f18d3936f346a34d1fb
TEMtheLEM <cinch01@outlook.com>
2022-08-30 19:29:08 -0400

    Change GitHub Dark -> GitHub Night

--- settings.php
+++ settings.php
@@ -66,7 +66,7 @@
                     <option value=\"nord\">Nord</option>
                     <option value=\"night_owl\">Night Owl</option>
                     <option value=\"discord\">Discord</option>
-                    <option value=\"github_dark\">Github Dark</option>";
+                    <option value=\"github_night\">GitHub Night</option>";
 
                     if (isset($_COOKIE["theme"]))
                     {
