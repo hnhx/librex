34dadcdefa9ed74232621bdc1dbe895f2309d668
Extravi <98912029+Extravi@users.noreply.github.com>
2022-08-31 15:05:02 -0400

    Update video.php

--- engines/google/video.php
+++ engines/google/video.php
@@ -1,15 +1,15 @@
 <?php
-    function get_video_results($query)
+    function get_video_results($query, $page=0)
     {
         global $config;
 
-        $url = "https://search.brave.com/videos?q=$query&source=web";
+        $url = "https://www.google.$config->google_domain/search?&q=$query&start=$page&hl=$config->google_language&tbm=vid";
         $response = request($url);
         $xpath = get_xpath($response);
 
         $results = array();
 
-        foreach($xpath->query("//div[@id='results']//div[contains(@class, 'card')]") as $result)
+        foreach($xpath->query("//div[@id='search']//div[contains(@class, 'g')]") as $result)
         {
             $url = $xpath->evaluate(".//a/@href", $result)[0];
 
@@ -24,8 +24,7 @@
             
             $url = check_for_privacy_frontend($url);
 
-            $title = $xpath->evaluate(".//div[contains(@class, 'title')]", $result)[0];
-            $thumbnail = urlencode($image->getAttribute(".//div[contains(@class, 'card-image']"));
+            $title = $xpath->evaluate(".//h3", $result)[0];
 
             array_push($results,
                 array (
@@ -48,14 +47,12 @@
                 $title = $result["title"];
                 $url = $result["url"];
                 $base_url = $result["base_url"];
-                $thumbnail = urlencode($result["thumbnail"]);
 
                 echo "<div class=\"text-result-wrapper\">";
                 echo "<a href=\"$url\">";
                 echo "$base_url";
                 echo "<h2>$title</h2>";
                 echo "</a>";
-                echo "<img src=\"image_proxy.php?url=$thumbnail\">";
                 echo "</div>";
             }
 
