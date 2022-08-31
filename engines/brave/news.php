e1510cba22a160f8f7ebba21e3b5349f980bd0e4
Extravi <98912029+Extravi@users.noreply.github.com>
2022-08-30 21:11:58 -0400

    Update news.php

--- engines/brave/news.php
+++ engines/brave/news.php
@@ -24,7 +24,7 @@
             
             $url = check_for_privacy_frontend($url);
 
-            $title = $xpath->evaluate(".//h3", $result)[0];
+            $title = $xpath->evaluate(".//span", $result)[0];
 
             array_push($results,
                 array (
