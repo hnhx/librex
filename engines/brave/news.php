42ff9515d0a7819d092cba576fce8ba6ca8639fc
Extravi <98912029+Extravi@users.noreply.github.com>
2022-08-30 20:02:43 -0400

    Add files via upload

---
+++ engines/brave/news.php
@@ -0,0 +1,61 @@
+<?php
+    function get_news_results($query)
+    {
+        global $config;
+
+        $url = "https://search.brave.com/news?q=$query&source=web";
+        $response = request($url);
+        $xpath = get_xpath($response);
+
+        $results = array();
+
+        foreach($xpath->query("//div[@id='results']//div[contains(@class, 'snippet')]") as $result)
+        {
+            $url = $xpath->evaluate(".//a/@href", $result)[0];
+
+            if ($url == null)
+                continue;
+
+            if (!empty($results)) // filter duplicate results
+                if (end($results)["url"] == $url->textContent)
+                    continue;
+
+            $url = $url->textContent;
+            
+            $url = check_for_privacy_frontend($url);
+
+            $title = $xpath->evaluate(".//h3", $result)[0];
+
+            array_push($results,
+                array (
+                    "title" => htmlspecialchars($title->textContent),
+                    "url" =>  htmlspecialchars($url),
+                    "base_url" => htmlspecialchars(get_base_url($url))
+                )
+            );
+        }
+
+        return $results;
+    }
+
+    function print_news_results($results)
+    {
+        echo "<div class=\"text-result-container\">";
+
+            foreach($results as $result)
+            {
+                $title = $result["title"];
+                $url = $result["url"];
+                $base_url = $result["base_url"];
+
+                echo "<div class=\"text-result-wrapper\">";
+                echo "<a href=\"$url\">";
+                echo "$base_url";
+                echo "<h2>$title</h2>";
+                echo "</a>";
+                echo "</div>";
+            }
+
+        echo "</div>";
+    }
+?>
