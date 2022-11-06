5ccee2ac1e1b2617f3b89590fb2e126b7176e783
vars1ty <narcotics@startmail.com>
2022-11-06 17:01:30 +0100

    Sync source with LinXer

--- donate.php
+++ donate.php
@@ -1,20 +1,18 @@
 <?php
-    require "misc/header.php";
+require "misc/header.php";
 
-    // Feel free to add your donation options here, but please don't remove mine.
+// Feel free to add your donation options here, but please don't remove mine.
 ?>
 
-    <title>LibreX - Donate</title>
-    </head>
-    <body>
-        <div class="misc-container">
-              <h1>Donate to the developer</h1>
-               <h3>Bitcoin (BTC):</h3>
-               <p>bc1qs43kh6tvhch02dtsp7x7hcrwj8fwe4rzy7lp0h</p>
-               <img src="static/images/btc.png" alt="btc qr code" width="150" height="150"/>
-               <h3>Monero (XMR):</h3>
-               <p>41dGQr9EwZBfYBY3fibTtJZYfssfRuzJZDSVDeneoVcgckehK3BiLxAV4FvEVJiVqdiW996zvMxhFB8G8ot9nBFqQ84VkuC</p>
-               <img src="static/images/xmr.png" alt="xmr qr code" width="150" height="150"/>
-        </div>
+<title>LibreX - Donate</title>
+</head>
+<body>
+<div class="misc-container">
+    <h1>Donate to the <a href="https://github.com/hnhx" target="_blank">LibreX Developer</a></h1>
+    <h3>Bitcoin (BTC):</h3>
+    <p>bc1qs43kh6tvhch02dtsp7x7hcrwj8fwe4rzy7lp0h</p>
+    <h3>Monero (XMR):</h3>
+    <p>41dGQr9EwZBfYBY3fibTtJZYfssfRuzJZDSVDeneoVcgckehK3BiLxAV4FvEVJiVqdiW996zvMxhFB8G8ot9nBFqQ84VkuC</p>
+</div>
 
 <?php require "misc/footer.php"; ?>
--- image_proxy.php
+++ image_proxy.php
@@ -1,24 +1,23 @@
 <?php
 
-    $config = require "config.php";
-    require "misc/tools.php";
+$config = require "config.php";
+require "misc/tools.php";
 
-    $url = $_REQUEST["url"];
+$url = $_REQUEST["url"];
 
-    $split_url = explode("/", $url);
-    $base_url = $split_url[2];
+$split_url = explode("/", $url);
+$base_url = $split_url[2];
 
-    $base_url_main_split = explode(".", strrev($base_url));
-    $base_url_main = strrev($base_url_main_split[1]) . "." . strrev($base_url_main_split[0]);
+$base_url_main_split = explode(".", strrev($base_url));
+$base_url_main = strrev($base_url_main_split[1]) . "." . strrev($base_url_main_split[0]);
 
-    $allowed_domains = array("qwant.com", "wikimedia.org", "brave.com");
+$allowed_domains = array("qwant.com", "wikimedia.org", "brave.com");
 
-    if (in_array($base_url_main, $allowed_domains))
-    {
-      $image = $url;
-      $image_src = request($image);
+if (in_array($base_url_main, $allowed_domains)) {
+    $image = $url;
+    $image_src = request($image);
 
-      header("Content-Type: image/jpeg");
-      echo $image_src;
-    }
+    header("Content-Type: image/jpeg");
+    echo $image_src;
+}
 ?>
