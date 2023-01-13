8dbc1757ff910524d5d3c01258248e65f045bb23
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-01-13 08:26:15 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -82,7 +82,6 @@
 
                             $frontends = array(
                                 "invidious" => array("https://docs.invidious.io/instances/", "YouTube"),
-                                "bibliogram" => array("https://git.sr.ht/~cadence/bibliogram-docs/tree/master/docs/Instances.md", "Instagram"),
                                 "nitter" => array("https://github.com/zedeus/nitter/wiki/Instances", "Twitter"),
                                 "libreddit" => array("https://github.com/spikecodes/libreddit", "Reddit"),
                                 "proxitok" => array("https://github.com/pablouser1/ProxiTok/wiki/Public-instances", "TikTok"),
