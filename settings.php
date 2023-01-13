41d26553188164d5895290577a84c23c647c9240
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-01-13 16:45:22 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -82,7 +82,11 @@
 
                             $frontends = array(
                                 "invidious" => array("https://docs.invidious.io/instances/", "YouTube"),
-                                "bibliogram" => array("https://git.sr.ht/~cadence/bibliogram-docs/tree/master/docs/Instances.md", "Instagram"),
+                                "hyperpipe" => array("https://hyperpipe.codeberg.page", "YouTube Music"),
+                                "rimgo" => array("https://codeberg.org/video-prize-ranch/rimgo#instances", "Imgur"),
+                                "scribe" => array("https://git.sr.ht/~edwardloveall/scribe/tree/main/docs/instances.md", "Medium"),
+                                "gothub" => array("https://codeberg.org/gothub/gothub/wiki/Instances", "GitHub"),
+                                "librarian" => array("https://codeberg.org/librarian/librarian#clearnet", "Odysee"),
                                 "nitter" => array("https://github.com/zedeus/nitter/wiki/Instances", "Twitter"),
                                 "libreddit" => array("https://github.com/spikecodes/libreddit", "Reddit"),
                                 "proxitok" => array("https://github.com/pablouser1/ProxiTok/wiki/Public-instances", "TikTok"),
