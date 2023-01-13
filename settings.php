ba193c5ad26682e19ed4a77ec2c713ee337cc8b1
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-01-13 08:42:29 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -81,7 +81,7 @@
                       <?php
 
                             $frontends = array(
-                                "invidious" => array("https://docs.invidious.io/instances/", "YouTube"),
+                                "invidious" => array("https://github.com/TeamPiped/Piped", "YouTube"),
                                 "bibliogram" => array("https://git.sr.ht/~cadence/bibliogram-docs/tree/master/docs/Instances.md", "Instagram"),
                                 "nitter" => array("https://github.com/zedeus/nitter/wiki/Instances", "Twitter"),
                                 "libreddit" => array("https://github.com/spikecodes/libreddit", "Reddit"),
