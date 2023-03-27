2283a4fee8c2b2e70f6a0d80796f13b4c68e68c3
Revvy <revvy@freedom>
2023-03-26 20:10:15 -0400

    suds => snopes

--- docker/attributes.sh
+++ docker/attributes.sh
@@ -46,6 +46,7 @@ export APP_QUETRE=${APP_QUETRE:-""}
 export APP_LIBREMDB=${APP_LIBREMDB:-""}
 export APP_BREEZEWIKI=${APP_BREEZEWIKI:-""}
 export APP_ANONYMOUS_OVERFLOW=${APP_ANONYMOUS_OVERFLOW:-""}
+export APP_SUDS=${APP_SUDS:-""}
 
 # GNU/Curl configurations. Leave 'CURLOPT_PROXY' blank whether you don't need to use a proxy for requests
 # Generally, a proxy is needed when your IP address is blocked by search engines in response to multiple requests within a short time frame. In these cases, it is recommended to use rotating proxies
